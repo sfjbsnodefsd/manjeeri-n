8244b123b9eccd9e65b063fa4f2e496e28f5d0b8
sfjbssept <sfjbssept@gmail.com>
2022-10-13 14:49:07 +0000

    addeed rest api call

--- pension/process-pension/app.js
+++ pension/process-pension/app.js
@@ -16,29 +16,54 @@ mongoose.connect("mongodb://localhost:27017/auth-service",{
   }
 );
 
+const getPensionerDetail = (aadhaarno) => 
+  new Promise((resolve, reject) => {
+    request({
+      url : `http://localhost:3002/pensioner/${aadhaarno}`,
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGhhcm5vIjoiMjM0NTY3ODk4NzY1IiwibmFtZSI6Ik1hbmplZXJpIiwiaWF0IjoxNjY1NjcxNTgyfQ.wtJus4TI8Q3kO56ERlEhfRtnIDrRsjazJkFgbehxPXk'
+      } 
+    },
+      (err, res, body) => {
+        if (err) {
+          return reject(err); 
+        }
+        resolve(JSON.parse(body));
+      }
+    );
+  });
+
 // calculate pension amount
-const calculatePension = async (adharno) => {
+const calculatePension = async (aadhaarno) => {
 
-  const pensionerDetail = await Pensioner.findOne({adharno:adharno}).select('salaryearned allowances pensiontype bankdetail');
+  const pensionerDetail = await getPensionerDetail(aadhaarno);
 
-  const bankCharge = {
-    'public' : 500,
-    'private' : 500
-  }
-  const bankType = pensionerDetail.bankdetail.banktype
-  const pensiontype = pensionerDetail.pensiontype
-  let pensionAmountPercentage = 0;
-  if(pensiontype == 'self'){
-    pensionAmountPercentage = 80;
-  }else{
-    pensionAmountPercentage = 20;
-  }
-  let pensionAmount =  (pensionAmountPercentage * pensionerDetail.salaryearned)/100 + pensionerDetail.allowances;
-  
-  return {
-    pensionAmount ,
-    bankServiceCharge : bankCharge[bankType]
-  }
+    if(pensionerDetail.sucess === 1){
+        return pensionerDetail
+    } else {
+      //const pensionerDetail = await Pensioner.findOne({adharno:adharno}).select('salaryearned allowances pensiontype bankdetail');
+
+      const bankCharge = {
+        'public' : 500,
+        'private' : 500
+      }
+      const bankType = pensionerDetail.bankdetail.banktype
+      const pensiontype = pensionerDetail.pensiontype
+      let pensionAmountPercentage = 0;
+      if(pensiontype == 'self'){
+        pensionAmountPercentage = 80;
+      }else{
+        pensionAmountPercentage = 20;
+      }
+      let pensionAmount =  (pensionAmountPercentage * pensionerDetail.salaryearned)/100 + pensionerDetail.allowances;
+      
+      return {
+        pensionAmount ,
+        bankServiceCharge : bankCharge[bankType]
+      }
+        
+    }
+
 }
 
 app.post("/ProcessPension", isAuthenticated, async (req, res) => {
