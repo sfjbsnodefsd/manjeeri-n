8a66416494b8507ae5ca79eff848a3e1ac3e7d1b
sfjbssept <sfjbssept@gmail.com>
2022-10-27 14:16:02 +0000

    navigation and routing

--- angular/helloWorldApp/src/app/app-routing.module.ts
+++ angular/helloWorldApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+}, {
+  path:"register",
+  component: UserFormComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
